4276986d86aaa5ed5210a31609b1d716762c23da
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2024-10-15 01:53:57 +0300

    feat: add error-message constants

---
+++ src/constants/errors.mjs
@@ -0,0 +1,10 @@
+const ERROR_MESSAGES = {
+  INVALID_INPUT: 'Invalid input',
+  OPERATION_FAILED: 'Operation failed',
+  INVALID_PATH: 'Invalid path by',
+  ALREADY_EXIST: 'already exist',
+  NOT_A_FILE: 'not a file',
+  NOT_A_DIRECTORY: 'not a directory',
+};
+
+export default ERROR_MESSAGES;
